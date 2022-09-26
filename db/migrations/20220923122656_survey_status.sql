e6a10fa4ddfa4ab30956991040ad64e3b83e9123
lee sock won <lsw4uto@gmail.com>
2022-09-26 09:02:40 +0900

    feat: dev modify survey_status

--- db/migrations/20220923122656_survey_status.sql
+++ db/migrations/20220923122656_survey_status.sql
@@ -3,7 +3,5 @@
 CREATE TABLE
     survey_status(
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        status VARCHAR(10),
-        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+        status VARCHAR(10)
     ) -- migrate:down
