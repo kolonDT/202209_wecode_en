2bbea8f355a5cdbd7b5e33d4976db0ccd10463b9
문정진 <jj@JJ-MacBookPro.local>
2022-09-29 10:51:08 +0900

    feat: retouch mainpage

--- models/mainDao.js
+++ models/mainDao.js
@@ -17,40 +17,6 @@ const getCount = async (adminPkId) => {
   }
 };
 
-const getAllList = async (adminPkId) => {
-  try {
-    return await database.query(
-      `
-      SELECT 
-        id,
-        DATE_FORMAT(start_date, '%Y%m%d') AS start_date,
-        DATE_FORMAT(end_date, '%Y%m%d') AS end_date,
-        survey_status_id
-      FROM survey
-      WHERE survey.admin_id = ?
-      AND NOT survey_status_id = 3`,
-      [adminPkId]
-    );
-  } catch (err) {
-    throw new error("INVALID_DATA_INPUT", 500);
-  }
-};
-
-const updateSurveyStatus = async (statusId, surveyId) => {
-  try {
-    await database.query(
-      `
-      UPDATE 
-        survey
-      SET survey_status_id = ? 
-      WHERE id = ?`,
-      [statusId, surveyId]
-    );
-  } catch (err) {
-    throw new error("INVALID_DATA_INPUT", 500);
-  }
-};
-
 const getList = async (startPageNo, limit, adminPkId) => {
   try {
     return await database.query(
@@ -80,7 +46,5 @@ const getList = async (startPageNo, limit, adminPkId) => {
 
 module.exports = {
   getCount,
-  getAllList,
-  updateSurveyStatus,
   getList,
 };
--- services/mainService.js
+++ services/mainService.js
@@ -5,23 +5,6 @@ const getCount = async (adminPkId) => {
   return getCount[0].surveycount;
 };
 
-const updateSurveyStatus = async (adminPkId) => {
-  const getAllList = await mainDao.getAllList(adminPkId);
-  let today = new Date();
-  let date = Number(
-    String(today.getFullYear()) +
-      String(("0" + (today.getMonth() + 1)).slice(-2)) +
-      String(("0" + today.getDate()).slice(-2))
-  );
-  getAllList.forEach((list) => {
-    if (date < Number(list.start_date)) {
-      mainDao.updateSurveyStatus(2, list.id);
-    } else if (date > Number(list.end_date)) {
-      mainDao.updateSurveyStatus(3, list.id);
-    }
-  });
-};
-
 const getList = async (pageNo, limit, adminPkId) => {
   const startPageNo = pageNo * limit - limit;
   const getList = await mainDao.getList(startPageNo, limit, adminPkId);
@@ -30,6 +13,5 @@ const getList = async (pageNo, limit, adminPkId) => {
 
 module.exports = {
   getCount,
-  updateSurveyStatus,
   getList,
 };
